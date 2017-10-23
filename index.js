8ad1d69a41c0c6b1f1ba3bf573c37b4d8a10f866
kieffer <nicolas.kieffer@inist.fr>
2017-10-23 16:58:13 +0200

    Fix bug tables

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let resources = {
   "template": fs.readFileSync(path.join(__dirname, "fragment.tei.xml.tpl"), 'utf-8')
 };
 
-for (let i = 0; i < resources.categorizations.length - 1; i++) {
+for (let i = 0; i < resources.categorizations.length; i++) {
   resources.tables[resources.categorizations[i].id] = require("./" + path.join("tables", resources.categorizations[i].table));
 }
 
